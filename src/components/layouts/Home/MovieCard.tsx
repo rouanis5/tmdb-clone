6b3c61ffa3afb42ec48c188eba3772f2177eded9
rouanis5 <ouanis42@gmail.com>
2024-04-07 15:13:15 +0100

    ceil movie card average vote

--- src/components/layouts/Home/MovieCard.tsx
+++ src/components/layouts/Home/MovieCard.tsx
@@ -1,5 +1,6 @@
 import { motion } from 'framer-motion'
 import { movieType } from '../../../types/tmdb.type'
+import { useMemo } from 'react'
 
 type movieCardType = Pick<
   movieType,
@@ -11,45 +12,49 @@ const MovieCard = ({
   release_date,
   poster_path,
   vote_average,
-}: movieCardType) => (
-  <motion.div
-    layout="position"
-    className="w-40 flex-shrink-0"
-    initial={{ opacity: 0 }}
-    animate={{ opacity: 1 }}
-    exit={{ opacity: 0 }}
-  >
-    <img
-      className="pointer-events-none h-60 w-full rounded-md bg-gray-300 shadow-sm"
-      loading="lazy"
-      src={'https://image.tmdb.org/t/p/w500' + poster_path}
-      alt={title + ' poster'}
-    />
-    <div
-      className="daisy-radial-progress ml-2 -mt-7 mb-2 border-4 border-black bg-black text-sm text-green-500"
-      style={
-        {
-          '--value': vote_average * 10,
-          '--size': '2.3rem',
-          '--thickness': '2px',
-        } as React.CSSProperties
-      }
+}: movieCardType) => {
+  const vote_percentage = useMemo(() => vote_average * 10, [vote_average])
+
+  return (
+    <motion.div
+      layout="position"
+      className="w-40 flex-shrink-0"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
     >
-      <span className="text-white">
-        {vote_average * 10}
-        <span className="mb-1 text-[.5em]">%</span>
-      </span>
-    </div>
-    <div className="ml-2 mr-1">
-      <a className="font-bold" href="#">
-        {title}
-      </a>
-      <br />
-      <a className="text-md font-light" href="#">
-        {release_date}
-      </a>
-    </div>
-  </motion.div>
-)
+      <img
+        className="pointer-events-none h-60 w-full rounded-md bg-gray-300 shadow-sm"
+        loading="lazy"
+        src={'https://image.tmdb.org/t/p/w500' + poster_path}
+        alt={title + ' poster'}
+      />
+      <div
+        className="daisy-radial-progress ml-2 -mt-7 mb-2 border-4 border-black bg-black text-sm text-green-500"
+        style={
+          {
+            '--value': vote_percentage,
+            '--size': '2.3rem',
+            '--thickness': '2px',
+          } as React.CSSProperties
+        }
+      >
+        <span className="text-white">
+          {Math.ceil(vote_percentage)}
+          <span className="mb-1 text-[.5em]">%</span>
+        </span>
+      </div>
+      <div className="ml-2 mr-1">
+        <a className="font-bold" href="#">
+          {title}
+        </a>
+        <br />
+        <a className="text-md font-light" href="#">
+          {release_date}
+        </a>
+      </div>
+    </motion.div>
+  )
+}
 
 export default MovieCard
