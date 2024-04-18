93d79f45abddeebf706e1fea1d58b47c00acc881
rouanis5 <ouanis42@gmail.com>
2024-04-18 03:53:56 +0100

    install shadcn

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,9 +11,23 @@ module.exports = {
       fontFamily: {
         sans: "'Source Sans Pro', sans-serif",
       },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
     },
   },
-  plugins: [require('daisyui')],
+  plugins: [require('daisyui'), require('tailwindcss-animate')],
   daisyui: {
     styled: true,
     themes: false,
