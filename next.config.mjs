da221c3b04b87c46c1bf0809461e76642cd4eaeb
rouanis5 <ouanis42@gmail.com>
2024-04-18 02:15:31 +0100

    next.config.mjs: remove output

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: undefined, // The default build output, .next directory, that works with production mode next start or a hosting provider like Vercel
   // output: 'export', // Outputs a Single-Page Application (SPA).
   distDir: './dist', // Changes the build output directory to `./dist/`.
 }
