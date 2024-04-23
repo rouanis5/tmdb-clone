9ac8f307b3e712ef8e006e480c6467b3716d3b3d
rouanis5 <ouanis42@gmail.com>
2024-04-23 12:31:56 +0100

    add movie likeBtn with a drawer

--- src/components/movies/MovieCard.tsx
+++ src/components/movies/MovieCard.tsx
@@ -1,27 +1,64 @@
 // import { motion } from 'framer-motion'
 import Image from 'next/image'
 import { movieType } from '@/types/tmdb.type'
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer'
+import { Button } from '@/components/ui/button'
+import { Heart } from 'lucide-react'
 
 type movieCardType = Pick<
   movieType,
   'title' | 'release_date' | 'poster_path' | 'vote_average'
 >
 
-const MovieCard = ({
+function LikeBtn(props: { className?: string; name: string }) {
+  return (
+    <span className={props.className}>
+      <Drawer>
+        <DrawerTrigger>
+          <span className="rounded-full size-7 bg-white grid place-items-center transition-colors hover:bg-neutral-200">
+            <Heart className="size-4 text-red-700" />
+          </span>
+        </DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>
+              Add `<i>{props.name}` to favorite movies </i>?
+            </DrawerTitle>
+            <DrawerDescription>
+              This action can be undone at any time.
+            </DrawerDescription>
+          </DrawerHeader>
+          <DrawerFooter>
+            <Button variant="secondary">Like</Button>
+            <DrawerClose>Cancel</DrawerClose>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </span>
+  )
+}
+
+function MovieCard({
   title,
   release_date,
   poster_path,
   vote_average,
-}: movieCardType) => {
+}: movieCardType) {
   return (
-    // <motion.div
-    //   layout="position"
-    //   className="w-40 flex-shrink-0"
-    //   initial={{ opacity: 0 }}
-    //   animate={{ opacity: 1 }}
-    //   exit={{ opacity: 0 }}
-    // >
-    <div className="w-40 flex-shrink-0">
+    <div className="w-40 flex-shrink-0 relative">
+      <LikeBtn
+        className="absolute right-0 top-0 translate-x-1/2 -translate-y-1/2"
+        name={title}
+      />
       <Image
         className="pointer-events-none h-60 w-full object-contain rounded-md bg-gray-300 shadow-sm"
         width={160}
